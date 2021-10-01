c840cbccdf6f253f8be89a6b8dc038cfeef129b4
luizhpv <luizventura03@gmail.com>
2021-10-01 01:11:57 -0300

    correção do LEIA

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var idade = 18;
 var participantes = 77;
 
 console.log('Digite a data de hoje:');
-Read(data_atual);
+console.read(data_atual);
 console.log('Digite a data do evento:');
-Read(data_evento);
+console.read(data_evento);
 if (data_atual < data_evento) {
     console.log('Evento Permitido!');
 } else {
     console.log('Data Invalida!');
 }
 console.log('Digite sua idade:');
-Read(idade);
+console.read(idade);
 if (idade >= 18) {
     console.log('Console Permitido!');
 } else {
     console.log('Idade não permitida!');
 }
 console.log('Digite a quantidade de participantes e palestrantes:');
-Read(participantes);
+console.read(participantes);
 if (participantes <= 100) {
     console.log('Quantidade permitida!');
 } else {
